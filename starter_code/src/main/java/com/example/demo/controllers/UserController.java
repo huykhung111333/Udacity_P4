e5d936194d984e869f9218c93bebfc0c89f3b8d8
huykhung111333 <56071171+huykhung111333@users.noreply.github.com>
2023-06-30 20:44:02 +0700

    First commit

---
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,76 @@
+package com.example.demo.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+	@Autowired
+	private final UserRepository userRepository;
+	
+	@Autowired
+	private final CartRepository cartRepository;
+	@Autowired
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+	public UserController(UserRepository userRepository, CartRepository cartRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userRepository = userRepository;
+		this.cartRepository = cartRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+
+	@GetMapping("/id/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.of(userRepository.findById(id));
+	}
+	
+	@GetMapping("/{username}")
+	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		log.debug("UserController.createUser: START ");
+		log.info("UserController.createUser: {}", createUserRequest.getUsername());
+		User user = new User();
+		user.setUsername(createUserRequest.getUsername());
+		Cart cart = new Cart();
+		cartRepository.save(cart);
+		user.setCart(cart);
+		if (createUserRequest.getPassword().length() < 8) {
+			log.info("Password can not less than 8 characters.");
+			log.info("UserController.createUser: END ");
+			return ResponseEntity.badRequest().build();
+		} else if (!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+			log.info("Password does not match");
+			log.info("UserController.createUser: END ");
+			return ResponseEntity.badRequest().build();
+		}
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+		userRepository.save(user);
+		log.info("UserController.createUser: Created user {} successfully.", createUserRequest.getUsername());
+		log.debug("UserController.createUser: END ");
+		return ResponseEntity.ok(user);
+	}
+	
+}
